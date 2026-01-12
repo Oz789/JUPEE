5bdb9acc283dbc7f2faff4c14c85bb0450c3b9b8
Oz789 <osvaldom5427@gmail.com>
2026-01-12 15:01:59 -0600

    Initial project setup

---
+++ ios/JUPEE/JUPEE/JUPEEApp.swift
@@ -0,0 +1,17 @@
+//
+//  JUPEEApp.swift
+//  JUPEE
+//
+//  Created by Osvaldo Mosso on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct JUPEEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
